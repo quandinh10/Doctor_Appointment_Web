a150962240ee1955ed0d39ba29783156e6709a2d
quannh <quannh@basebs.com>
2023-12-08 09:00:49 +0700

    rm password db

--- DB_config/db_config.php
+++ DB_config/db_config.php
@@ -4,8 +4,7 @@ return [
     "username" => "root",
     "password" => "",
     "doctor_email" => "quan.nguyen2711@hcmut.edu.vn",
-    "default_dr_password" => "superdoctor123",
-    "password" => "123"
+    "default_dr_password" => "superdoctor123"
 ]
 
 ?>
