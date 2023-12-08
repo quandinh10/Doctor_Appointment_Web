8ec2f76fa51360c5eec9a7058dd90f279d0bc43d
quandinh10 <dinhquan1003@gmail.com>
2023-12-08 08:02:00 +0700

    format

--- update_timeslot_status.php
+++ update_timeslot_status.php
@@ -10,10 +10,10 @@ if (isset($_POST["date"]) && isset($_POST["day"]) && isset($_POST["status"]) && 
     $status = $_POST["status"];
     $DAYS_OF_WEEK = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
     $day = $DAYS_OF_WEEK[$day];
-    
+
     $query = "SELECT SlotID FROM slot WHERE Date = '{$date}' AND DayofWeek = '{$day}' AND TimeSlot = '{$timeslot}'";
     $res = $mysqli->query($query);
-    
+
     if ($res->num_rows > 0) {
         $row = $res->fetch_assoc();
         $slotid = $row["SlotID"];
@@ -23,15 +23,14 @@ if (isset($_POST["date"]) && isset($_POST["day"]) && isset($_POST["status"]) && 
         $status_res_row = $status_res->fetch_assoc();
         $slotStatus = $status_res_row['Status'];
 
-        if($status === 'busy') {
-            if($slotStatus === 'busy')
-            {
-                if($role ===  'doctor') {
+        if ($status === 'busy') {
+            if ($slotStatus === 'busy') {
+                if ($role ===  'doctor') {
                     $sql = "SELECT PatientID FROM slot WHERE SlotID = $slotid";
                     $patient_res = $mysqli->query($sql);
                     $patient_row = $patient_res->fetch_assoc();
                     $patientID = $patient_row['PatientID'];
-                    if($patientID) {
+                    if ($patientID) {
                         $response = ['success' => false, 'message' => 'You have an appointment with patient ' . $patientID . '.'];
                         header('Content-Type: application/json');
                         echo json_encode($response);
@@ -39,26 +38,24 @@ if (isset($_POST["date"]) && isset($_POST["day"]) && isset($_POST["status"]) && 
                         $response = ['success' => false, 'message' => 'You are busy at this time.'];
                         header('Content-Type: application/json');
                         echo json_encode($response);
-                    }   
-                } elseif($role === 'patient') {
+                    }
+                } elseif ($role === 'patient') {
                     $response = ['success' => false, 'message' => 'Doctor is busy at this time.'];
                     header('Content-Type: application/json');
                     echo json_encode($response);
                 }
-            } elseif($slotStatus === 'available') {
-                if($role === 'doctor') {
+            } elseif ($slotStatus === 'available') {
+                if ($role === 'doctor') {
                     $update_sql = "UPDATE slot SET Status = '$status' WHERE SlotID = $slotid";
                     $update = $mysqli->query($update_sql);
                     $response = ['success' => true, 'message' => 'Timeslot status updated successfully.'];
                     header('Content-Type: application/json');
                     echo json_encode($response);
-                }
-                elseif($role === 'patient') {
+                } elseif ($role === 'patient') {
                     $sql = "SELECT availableSlot FROM user WHERE ID = '$id'";
                     $avaiSlot_res = $mysqli->query($sql);
                     $avaiSlot_row = $avaiSlot_res->fetch_assoc();
-                    if($avaiSlot_row['availableSlot'] == 0)
-                    {
+                    if ($avaiSlot_row['availableSlot'] == 0) {
                         $response = ['success' => false, 'message' => 'You are out of available slot.'];
                         header('Content-Type: application/json');
                         echo json_encode($response);
@@ -73,13 +70,13 @@ if (isset($_POST["date"]) && isset($_POST["day"]) && isset($_POST["status"]) && 
                     }
                 }
             }
-        } elseif($status === 'available') {
-            if($slotStatus === 'busy') {
+        } elseif ($status === 'available') {
+            if ($slotStatus === 'busy') {
                 $sql = "SELECT PatientID FROM slot WHERE SlotID = $slotid";
                 $patient_res = $mysqli->query($sql);
                 $patient_row = $patient_res->fetch_assoc();
                 $patientID = $patient_row['PatientID'];
-                if($patientID) {
+                if ($patientID) {
                     $response = ['success' => false, 'message' => 'You have an appointment with patient ' . $patientID . '.'];
                     header('Content-Type: application/json');
                     echo json_encode($response);
@@ -89,20 +86,20 @@ if (isset($_POST["date"]) && isset($_POST["day"]) && isset($_POST["status"]) && 
                     $response = ['success' => true, 'message' => 'Timeslot status updated successfully.'];
                     header('Content-Type: application/json');
                     echo json_encode($response);
-                } 
-            } elseif($slotStatus === 'available') {
+                }
+            } elseif ($slotStatus === 'available') {
                 $response = ['success' => false, 'message' => 'You have already set available at this time.'];
                 header('Content-Type: application/json');
                 echo json_encode($response);
             }
-        } elseif($status === 'cancel') {
-            if($slotStatus === 'busy') {
+        } elseif ($status === 'cancel') {
+            if ($slotStatus === 'busy') {
                 $sql = "SELECT PatientID FROM slot WHERE SlotID = $slotid";
                 $patient_res = $mysqli->query($sql);
                 $patient_row = $patient_res->fetch_assoc();
                 $patientID = $patient_row['PatientID'];
-                if($role === 'doctor'){
-                    if($patientID) {
+                if ($role === 'doctor') {
+                    if ($patientID) {
                         $sql = "UPDATE user SET availableSlot = availableSlot + 1 WHERE ID = '$patientID'";
                         $reduce = $mysqli->query($sql);
                         $sql = "UPDATE slot SET Status = 'available', PatientID = NULL WHERE SlotID = $slotid";
@@ -114,10 +111,10 @@ if (isset($_POST["date"]) && isset($_POST["day"]) && isset($_POST["status"]) && 
                         $response = ['success' => false, 'message' => 'You don\'t have any appointments at this time.'];
                         header('Content-Type: application/json');
                         echo json_encode($response);
-                    }    
-                } elseif($role === 'patient') {
-                    if($patientID) {
-                        if($id === $patientID) {
+                    }
+                } elseif ($role === 'patient') {
+                    if ($patientID) {
+                        if ($id === $patientID) {
                             $sql = "UPDATE user SET availableSlot = availableSlot + 1 WHERE ID = '$patientID'";
                             $reduce = $mysqli->query($sql);
                             $sql = "UPDATE slot SET Status = 'available', PatientID = NULL WHERE SlotID = $slotid";
@@ -136,12 +133,12 @@ if (isset($_POST["date"]) && isset($_POST["day"]) && isset($_POST["status"]) && 
                         echo json_encode($response);
                     }
                 }
-            } elseif($slotStatus === 'available') {
-                if($role === 'doctor') {
+            } elseif ($slotStatus === 'available') {
+                if ($role === 'doctor') {
                     $response = ['success' => false, 'message' => 'There are no appointments at this time.'];
                     header('Content-Type: application/json');
                     echo json_encode($response);
-                } elseif($role === 'patient') {
+                } elseif ($role === 'patient') {
                     $response = ['success' => false, 'message' => 'You can not edit the timeslot that are not your appointment.'];
                     header('Content-Type: application/json');
                     echo json_encode($response);
@@ -149,20 +146,19 @@ if (isset($_POST["date"]) && isset($_POST["day"]) && isset($_POST["status"]) && 
             }
         }
     } else {
-        if($status === 'busy' || $status === 'available')
-        {
-            if($role ===  'doctor'){
+        if ($status === 'busy' || $status === 'available') {
+            if ($role ===  'doctor') {
                 $sql = "INSERT INTO slot (Date, DayofWeek, TimeSlot, Status) VALUES ('$date', '$day', '$timeslot', '$status')";
                 $update = $mysqli->query($sql);
                 $response = ['success' => true, 'message' => 'Timeslot status updated successfully.'];
                 header('Content-Type: application/json');
                 echo json_encode($response);
-            } elseif($role === 'patient') {
+            } elseif ($role === 'patient') {
                 $response = ['success' => false, 'message' => 'Patient can\'t setup new timeslot.'];
                 header('Content-Type: application/json');
                 echo json_encode($response);
             }
-        } elseif($status === 'cancel') {
+        } elseif ($status === 'cancel') {
             $response = ['success' => false, 'message' => 'This timeslot doesn\'t created.'];
             header('Content-Type: application/json');
             echo json_encode($response);
